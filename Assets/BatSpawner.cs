6a49aa558466785c744fb367ed78bea83ebe4370
hwallace315 <wallacehope1@gmail.com>
2015-09-25 19:55:37 -0400

    Shooting works now!

---
+++ Assets/BatSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatSpawner : MonoBehaviour {
+
+    public GameObject bat;
+    public float radius;
+    public float spawnTime = 3f;
+
+	// Use this for initialization
+	void Start () {
+
+        InvokeRepeating("SpawnBat", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void SpawnBat()
+    {
+        GameObject newBat = Instantiate(bat);
+        float angle = getRandomAngle();
+        float x = radius * Mathf.Cos(angle) + transform.position.x;
+        float y = radius * Mathf.Sin(angle) + transform.position.y;
+        Vector2 vector = new Vector2(x, y);
+
+        newBat.transform.position = vector;
+    }
+
+    float getRandomAngle()
+    {
+        return Random.Range(0.0F, 360.0F);
+    }
+}
--- Assets/Bats.cs
+++ Assets/Bats.cs
@@ -3,34 +3,22 @@ using System.Collections;
 
 public class Bats : MonoBehaviour {
 
-    public GameObject bat;
-    public float radius;
-    public float spawnTime = 3f;
-
 	// Use this for initialization
 	void Start () {
-
-        InvokeRepeating("SpawnBat", spawnTime, spawnTime);
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+	
 	}
 
-    void SpawnBat()
+    void OnCollisionEnter(Collision col)
     {
-        GameObject newBat = Instantiate(bat);
-        float angle = getRandomAngle();
-        float x = radius * Mathf.Cos(angle) + transform.position.x;
-        float y = radius * Mathf.Sin(angle) + transform.position.y;
-        Vector2 vector = new Vector2(x, y);
-
-        newBat.transform.position = vector;
-    }
-
-    float getRandomAngle()
-    {
-        return Random.Range(0.0F, 360.0F);
+        if(col.gameObject.name.Contains("bat") && col.collider.name.Contains("bullet"))
+        {
+            Destroy(col.gameObject);
+            Destroy(col.collider.gameObject);
+        }
     }
 }
